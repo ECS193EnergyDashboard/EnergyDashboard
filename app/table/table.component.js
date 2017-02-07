085c809b31165e3f5a1990e29d9a30d56501f0ec
Billy Herr <THEKING@campus-050-027.ucdavis.edu>
2017-02-07 12:41:48 -0800

    hard coded table not good but works

--- app/table/table.component.js
+++ app/table/table.component.js
@@ -5,8 +5,18 @@ angular.module('tableModule').component('table',{
 		var self = this;
 		// self.orderProp = 'carrier';
 
-		$http.get('json/table-fake.json').success(function(response){
-			self.rooms = response.Rooms;
+		$http.get('json/use.json').then(function(response){
+			self.rooms = response.data;
+			self.values = response.data.Values;
+
+
+	    // $scope.artists = [];
+	    // angular.forEach(data.artists, function(value, key) {
+	    //     $scope.artists.push(value);
+	    // });
+	    // $scope.isVisible = function(name){
+	    //     return true;// return false to hide this artist's albums
+	    // };
 
 			// $scope.artists = data.artists; // response data 
 			// $scope.albums = data.artists.albums; /this is where im getting trouble
@@ -17,20 +27,32 @@ angular.module('tableModule').component('table',{
 });
 
 
-
-
-
-
 // angular.module('tableModule').component('table',{
 // 	templateUrl: 'table/table.template.html',
-// 	controller: function TableController($scope){
-// 		$scope.rowCollection = [{
-// 			firstName:"home",
-// 			lastName:"poop"
-// 		}, {
-// 			firstName:"stewie",
-// 			lastName:"poop"
-// 		}];
-// 	}
+// 	controller: ['$http', function TableController($http){
+// 		var self = this;
+// 		// self.orderProp = 'carrier';
+
+// 		$http.get('json/table-fake.json').then(function(response){
+// 			self.rooms = response.data;
+// 			//self.values = response.Rooms.Values
+
+
+// 	    // $scope.artists = [];
+// 	    // angular.forEach(data.artists, function(value, key) {
+// 	    //     $scope.artists.push(value);
+// 	    // });
+// 	    // $scope.isVisible = function(name){
+// 	    //     return true;// return false to hide this artist's albums
+// 	    // };
+
+// 			// $scope.artists = data.artists; // response data 
+// 			// $scope.albums = data.artists.albums; /this is where im getting trouble
+
+
+// 		})
+// 	}]
 // });
+
+
 

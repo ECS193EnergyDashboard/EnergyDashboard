da080444160633639bcc55dbba5446ac9b1c2b58
Anthony Kacyra <akacyra@gmail.com>
2017-02-08 18:34:46 -0800

    Removed test table.

--- app/core/sensor/sensor.service.js
+++ app/core/sensor/sensor.service.js
@@ -1,22 +1,3 @@
-var postData = {
-	rooms: {
-		Method: "GET",
-		Resource: "https://ucd-pi-iis.ou.ad3.ucdavis.edu/piwebapi/elements/E0bgZy4oKQ9kiBiZJTW7eugwLaXd-Olm5RGZEkhRt5d2AAVVRJTC1BRlxBQ0VcVUMgREFWSVNcQlVJTERJTkdTXEdIQVVTSVxTVUJTWVNURU1cQUhVXEFIVTAx/elements?selectedFields=Items.Name;Items.WebId"
-	},
-	values: {
-		Method: "GET",
-		RequestTemplate: {
-			"Resource": "https://ucd-pi-iis.ou.ad3.ucdavis.edu/piwebapi/streamsets/{0}/value?selectedFields=Items.Value.Value;Items.Value.UnitsAbbreviation;Items.Value.Good"
-		},
-		Parameters: [
-			"$.rooms.Content.Items[*].WebId"
-		],
-		ParentIds: [
-			"rooms"
-		]
-	}
-}
-
 angular.
     module('core.sensor').
     factory('sensor', ['$http', '$q',
@@ -41,21 +22,21 @@ angular.
                         url = base + 'streamsets/' + rooms[i].WebId + '/value?selectedFields=Items.Name;Items.Value.Value;Items.Value.UnitsAbbreviation;Items.Value.Good'
                         promises.push($http.get(url));
                     }
-                    $q.all(promises).then(function(responses) {
+                    return $q.all(promises).then(function(responses) {
                         for (var i = 0; i < responses.length; i++) {
                             var values = responses[i].data.Items;
                             for (value of values) {
+                                // TODO: set defaults
                                 var v = value.Value;
                                 if (v.Value.Value !== undefined) { // Handle nested value objects
                                     v.Value = v.Value.Value;
                                 }
-                                console.log(v.Value);
                                 v.Name = value.Name;
                                 data.Rooms[i].Values.push(v);
                             }
                         }
+                        return data;
                     });
-                    return data;
                 })
               }  
             };

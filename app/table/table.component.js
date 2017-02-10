58c19ff2f1a062a43f018c9dc2d611ef817b3a9d
Anthony Kacyra <akacyra@gmail.com>
2017-02-10 02:38:34 -0800

    Can sort and search by Room. Sort still broken for values. Bad values have red backgrounds.

--- app/table/table.component.js
+++ app/table/table.component.js
@@ -3,14 +3,32 @@ angular.module('tableModule').component('roomTable',{
 	templateUrl: 'table/table.template.html',
 	controller: ['$http', '$filter', 'sensor', function TableController($http, $filter, sensor){
 		var self = this;	
-		var rooms = [];
-		var valueNames = [];
+		this.rooms = [];
 
 		sensor.query().then(function(data){
 			self.rooms = data.Rooms;
-			self.valueNames = self.rooms[0].Values;
   			console.log(data);
 		});
+
+		this.getters = {
+			value: function(room, index) {
+				return room.Values[index].Value;
+			}
+		}
+
+		this.formatValue = function(value, decimals) {
+			var decimals = decimals || 2;
+			var result = "";
+			if (value.Good && typeof(value.Value) === "number") {
+				result += $filter('number')(value.Value, decimals);
+				if (value.UnitsAbbreviation) {
+					result += " " + value.UnitsAbbreviation;
+				}
+			} else {
+				result += value.Value;
+			}
+			return result;
+		}
 
 	}]
 });

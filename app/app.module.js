a0d36e756757071b248e115e0651fb8a4a4df218
Christian Aranda <charanda@ucdavis.edu>
2017-02-10 12:56:16 -0800

    Sidebar fixes

--- app/app.module.js
+++ app/app.module.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-angular.module('energyDashboard', [
-    'menuBar',
-    'table',
-    'sideBar'
-]);
-=======
 angular.module('energyDashboardModule', [
     'tableModule',
     'menuBarModule',
     'core',
     'smart-table',
-    'sideBar'
+    'sideNavModule'
 ]);
->>>>>>> 58c19ff2f1a062a43f018c9dc2d611ef817b3a9d

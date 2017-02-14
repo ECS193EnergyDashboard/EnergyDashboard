499ab8dd2b1057ad4d82c7c1027b663e55a998a1
Cuauhtemoctzin Rodriguez <ctrodriguez@ucdavis.edu>
2017-02-13 20:13:04 -0800

    Added some html to start the heirarchy. Added a controller to the side-nav that uses the Pi service to grab a list of buildings. The buildings are displayed on the side-nav. TODO: when the user clicks it opens up.

--- app/side-nav/side.component.js
+++ app/side-nav/side.component.js
@@ -1,6 +1,15 @@
 
 angular.module('sideNavModule').component('sideBar',{
-	templateUrl: 'side-nav/side.template.html'
+	templateUrl: 'side-nav/side.template.html',
+    controller: ['$scope', 'pi', function TableController($scope, pi){
+
+        // webid for buildings list
+        var webId = 'E0bgZy4oKQ9kiBiZJTW7eugwDBxX8Kms5BG77JiQlqSuWwVVRJTC1BRlxBQ0VcVUMgREFWSVNcQlVJTERJTkdT';
+        pi.getValuesOfChildren(webId).then(function(data){
+			$scope.buildings = data.elements;
+  			console.log("buildings: ", $scope.buildings);
+		});
+
+
+	}]
 });
-
-

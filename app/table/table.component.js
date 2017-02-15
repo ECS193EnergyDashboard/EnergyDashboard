af9949ebfafed90ce07bee497084c74a3c727bf0
Billy Herr <THEKING@campus-052-207.ucdavis.edu>
2017-02-14 16:51:53 -0800

    Reorderable collumns works and the code is cleaned up however, it broke the search function

--- app/table/table.component.js
+++ app/table/table.component.js
@@ -36,45 +36,10 @@
 
 angular.module('tableModule').component('roomTable',{
 	templateUrl: 'table/table.template.html',
-	controller: ['$http', '$filter', 'sensor', '$scope', function TableController($http, $filter, sensor, $scope){
+	controller: ['$http', '$filter', 'sensor', function TableController($http, $filter, sensor){
 
-		var self = this;	
-		this.rooms = [];
-
-		sensor.query().then(function(data){
-			self.rooms = data.Rooms;
-
-  			$scope.columns = data.Rooms[0].Values;
-
-	        $scope.rowCollection = [];
-
-
-	        function parseJSON(room) {
-	                var values = {}
-
-	                values["name"] = self.rooms[room].Name;
-	                for(var i=0;i<$scope.columns.length;i++){
-	                	values[$scope.columns[i].Name] = self.rooms[room].Values[i].Value;
-	                }
-	                return values;
-
-	        }
-	        
-
-	        for(var i=0;i<self.rooms.length;i++){
-	          $scope.rowCollection.push(parseJSON(i));
-	        }
-
-
-		});
-
-		this.getters = {
-			value: function(room, index) {
-				return room.Values[index].Value;
-			}
-		}
-
-		this.formatValue = function(value, decimals) {
+		//Function to formate the values with their units
+		function formatValue(value, decimals) {
 			var decimals = decimals || 2;
 			var result = "";
 			if (value.Good && typeof(value.Value) === "number") {
@@ -89,6 +54,44 @@ angular.module('tableModule').component('roomTable',{
 		}
 
 
+		var self = this;	
+		this.rooms = [];
+
+		// Use sensor to query for the data
+		sensor.query().then(function(data){
+			self.rooms = data.Rooms;
+
+			// Columns variable used just in javascript
+			columns = self.rooms[0].Values;
+
+	        self.rowCollection = [];
+
+
+	        function parseJSON(room) {
+	            var values = {}
+
+	            values["RoomNumber"] = self.rooms[room].Name;
+	            for(var i=0;i<columns.length;i++){
+	            	values[columns[i].Name] = formatValue(self.rooms[room].Values[i]);
+	            }
+	            return values;
+	        }
+	        
+	        for(var i=0;i<self.rooms.length;i++){
+	          self.rowCollection.push(parseJSON(i));
+	        }
+
+
+		});
+
+
+		// Getter function
+		this.getters = {
+			value: function(room, index) {
+				return room.Values[index].Value;
+			}
+		}
+		
 
 	}]
 });

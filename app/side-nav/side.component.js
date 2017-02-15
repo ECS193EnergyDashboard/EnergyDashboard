3673309bb59c19c2ef6fbc360da47c081943a4d0
Cuauhtemoctzin Rodriguez <ctrodriguez@ucdavis.edu>
2017-02-14 16:12:21 -0800

    Made the heirarchy collapsable. Added a plus glyphicon. TODO: add minus glyphicon.

--- app/side-nav/side.component.js
+++ app/side-nav/side.component.js
@@ -1,22 +1,31 @@
-
-angular.module('sideNavModule').component('sideBar',{
-	templateUrl: 'side-nav/side.template.html',
-    controller: ['$scope', 'pi', function TableController($scope, pi){
+angular.module('sideNavModule').component('sideBar', {
+    templateUrl: 'side-nav/side.template.html',
+    controller: ['$scope', 'pi', function TableController($scope, pi) {
 
         // webid for buildings list
         var webId = 'E0bgZy4oKQ9kiBiZJTW7eugwDBxX8Kms5BG77JiQlqSuWwVVRJTC1BRlxBQ0VcVUMgREFWSVNcQlVJTERJTkdT';
-        pi.getValuesOfChildren(webId).then(function(data){
-			$scope.buildings = data.elements;
-  			console.log("buildings: ", $scope.buildings);
-		});
+        pi.getValuesOfChildren(webId).then(function(data) {
+            $scope.buildings = data.elements;
+            console.log("buildings: ", $scope.buildings);
+        });
 
-        $scope.clickElem = function(element){
-            pi.getValuesOfChildren(element.webId).then(function(data){
-    			element.elements = data.elements;
-      			console.log("clicked: " + element.name);
-                console.log(element.name +" data", data.elements);
-    		});
+        $scope.clickElem = function(element) {
+            if (element.elements == null || element.elements === undefined) {
+                pi.getValuesOfChildren(element.webId).then(function(data) {
+                    element.elements = data.elements;
+                    console.log("clicked: " + element.name);
+                    //console.log(element.name +" data", data.elements);
+                });
+            }
+
+            // element does not have show property yet
+            if (element.show == null || element.show === undefined) {
+                element.show = true;
+            } else {
+                element.show = !element.show;
+            }
+
         }
 
-	}]
+    }]
 });

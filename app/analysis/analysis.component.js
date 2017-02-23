ca4ea75851a9e718629ce04c022842dc0690243b
Cuauhtemoctzin Rodriguez <ctrodriguez@ucdavis.edu>
2017-02-22 16:02:22 -0800

    Added analysis function checkboxes.

--- app/analysis/analysis.component.js
+++ app/analysis/analysis.component.js
@@ -4,11 +4,39 @@ angular.module('analysisModule').component('analysis', {
         tableSrc: '<'
     },
     controller: ['$filter', 'pi', function TableController($filter, pi) {
-        console.log("analysis controller");
+        this.columnNamesObjs = [{
+                name: "avg",
+                isDefault: true,
+                isChecked: true
+            },
+            {
+                name: "max",
+                isDefault: true,
+                isChecked: true
+            },
+            {
+                name: "min",
+                isDefault: true,
+                isChecked: true
+            }, {
+                name: "std dev",
+                isDefault: true,
+                isChecked: true
+            }
+        ];
+        this.columnNames = this.columnNamesObjs.map(function(a) {return a.name;});
 
-    /*    for (var element of this.tableSrc) {
-            self.data.push(parseJSON(element));
-        }*/
+        this.startAnalysis = function(){
+            // compute analysis
+            //update table
+        };
+
+        /*    for (var element of this.tableSrc) {
+                self.data.push(parseJSON(element));
+            }*/
+        this.$onChanges = function(){
+            console.log("analysis tableSrc: ", this.tableSrc); //not working
+        };
 
     }]
 });

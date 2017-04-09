1070da1b66f35126d899c62cd25af0352d816ba4
Billy Herr <weherr@ucdavis.edu>
2017-04-09 16:11:47 -0700

    CSV download now works on the analysis tab

--- app/column-template-dropdown/column-template-dropdown.component.js
+++ app/column-template-dropdown/column-template-dropdown.component.js
@@ -5,6 +5,7 @@ angular.module('columnTemplateDropdownModule').component('columnTemplateDropdown
         rowData: '<',
         isAnalysis: '<',
         templateSets: '<',
+        innerColumns: '<', // Min, max, avg, st - this is needed for CSV
         updateColObj: '&'
     },
     controller: [ '$http', function colTemplateController($http) {
@@ -15,7 +16,6 @@ angular.module('columnTemplateDropdownModule').component('columnTemplateDropdown
 
 
             this.$onChanges = function() {
-                //console.log("columnTemplateDropdown cols : ", this.columns);
 
                 // Get templates from server
                 $http({method: 'GET', url: '/getTemplates'}).then(function successCallback(response) {
@@ -25,13 +25,11 @@ angular.module('columnTemplateDropdownModule').component('columnTemplateDropdown
                     console.error("get templates failed ", response);
                 });
 
-                console.log("is analysis", this.isAnalysis);
-
             };
 
 
 
-            this.GetHeader = function() {
+            this.GetHeaderData = function() {
                 columnNames = [];
                 columnNames.push("Name");
                 columnNames.push("Building");
@@ -61,21 +59,92 @@ angular.module('columnTemplateDropdownModule').component('columnTemplateDropdown
             }
 
 
+            this.GetHeaderAnalysis = function() {
+                var numInnerColumns = 0;
+                if(this.innerColumns[0].isChecked == true)
+                    numInnerColumns++; // Blank space for Avg
+                if(this.innerColumns[1].isChecked == true)
+                    numInnerColumns++; // Blank space for Max
+                if(this.innerColumns[2].isChecked == true)
+                    numInnerColumns++; // Blank space for Min
+                if(this.innerColumns[3].isChecked == true)
+                    numInnerColumns++; // Blank space for S.D.
+
+                var columnNames = [];
+                columnNames.push(" "); // Blank space for Name
+                columnNames.push(" "); // Blank space for Building
+                for (var element of this.columns){
+                    if(element.isChecked){
+                        columnNames.push(element.name);
+                        for(var i=0; i<numInnerColumns - 1; i++)
+                            columnNames.push(" ");
+                    }
+                }
+                return columnNames;
+            };
+
+
             // ASK JUSTIN: Should this display units? on column name or in rows?
             this.GetArrayAnalysis = function() {
                 var CSVData = [];
-                console.log(this.rowData);
-                // for(var element of this.rowData){
-                //     var obj = {};
-                //     obj.name = element.name;
-                //     obj.building = element.building;
-                //     for(var col of this.columns){
-                //         if(col.isChecked){
-                //             obj[col.name] = element[col.name].value;
-                //         }
-                //     }
-                //     CSVData.push(obj);
-                // }
+
+                // create a second row which will have labels for min, max, et...
+                var secondCol = [];
+                secondCol.push("Name");
+                secondCol.push("Building");
+                for(var col of this.columns){
+                    if(col.isChecked){
+                        if(this.innerColumns[0].isChecked == true)
+                            secondCol.push("AVG"); // Blank space for Avg
+                        if(this.innerColumns[1].isChecked == true)
+                            secondCol.push("MAX"); // Blank space for Max
+                        if(this.innerColumns[2].isChecked == true)
+                            secondCol.push("MIN"); // Blank space for Min
+                        if(this.innerColumns[3].isChecked == true)
+                            secondCol.push("S.D."); // Blank space for S.D.
+                    }                    
+                }
+                CSVData.push(secondCol);
+
+                // set the other rows of the CSV file
+                for(var element of this.rowData){
+                    var elmRow = [];
+                    elmRow.push(element.name);
+                    elmRow.push(element.building);
+                    for(var col of this.columns){
+                        if(col.isChecked){
+
+                            // Check Avg value is good
+                            if(element[col.name].Average.good)
+                                elmRow.push(element[col.name].Average.value);
+                                // [-11059] No Good Data For Calculation
+                            else
+                                elmRow.push("Bad Value");
+
+                            // check Max value is good
+                            if(element[col.name].Maximum.good)
+                                elmRow.push(element[col.name].Maximum.value);
+                            else
+                                elmRow.push("Bad Value");
+
+                            // Check min value is good
+                            if(element[col.name].Minimum.good)
+                                elmRow.push(element[col.name].Minimum.value);
+                            else
+                                elmRow.push("Bad Value");
+
+                            // check StdDev value is good
+                            if(element[col.name].StdDev.good)
+                                elmRow.push(element[col.name].StdDev.value);
+                            else
+                                elmRow.push("Bad Value");
+                        }
+                    }
+
+                    CSVData.push(elmRow);
+
+                }
+
                 return CSVData;
             }
 
@@ -112,11 +181,8 @@ angular.module('columnTemplateDropdownModule').component('columnTemplateDropdown
             };
 
 
-            this.DownloadCSV = function(){
-                console.log('DOwnloading CSV');
-            };
 
-
+            // Default file name for downloading to CSV
             this.fileName = "Data.csv";
 
 

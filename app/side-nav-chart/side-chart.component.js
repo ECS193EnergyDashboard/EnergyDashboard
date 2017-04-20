8f31fa96ca8827440e999f7f71d0aa3efed4e055
Christian Aranda <charanda@ucdavis.edu>
2017-04-19 20:21:51 -0700

    Bugfixes: Data table duplicates, side bar scroll, text fixes, disabled template, fixed graphing

--- app/side-nav-chart/side-chart.component.js
+++ app/side-nav-chart/side-chart.component.js
@@ -1,12 +1,6 @@
 angular.module('sideNavChartModule').component('sideBarChart', {
     templateUrl: 'side-nav-chart/side-chart.template.html',
     bindings: {
-        itemsToAdd: '<',
-        onSelect: '&',
-        onDeselect: '&',
-        onClear: '&',
-        loading: '<',
-        onClick: '&',
         selection: '<'
     },
     controller: [ 'pi', function TableController(pi) {
@@ -42,26 +36,6 @@ angular.module('sideNavChartModule').component('sideBarChart', {
         this.isDummyItem = function(e) {
             return -1 === e.numId;
         };
-
-        this.onDropSuccess = function(element) {
-            this.onSelect({ item: element });
-        };
-
-        this.$onChanges = function(changes) {
-            if (changes.itemsToAdd) {
-
-                var set = { };
-                for (var item of this.selected) {
-                    set[item.numId] = true;
-                }
-                
-                for (var item of this.itemsToAdd) {
-                    if (!set[item.numId])
-                    this.selected.splice(this.selected.length - 1, 0, item);
-                    this.onDropSuccess(item);
-                }
-            }
-        }
 
     }]
 });
--- app/side-nav-chart/side-chart.module.js
+++ app/side-nav-chart/side-chart.module.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Define the 'sideNavModule' module
-angular.module('sideNavChartModule', ['customDragNDrop']);
+angular.module('sideNavChartModule', []);

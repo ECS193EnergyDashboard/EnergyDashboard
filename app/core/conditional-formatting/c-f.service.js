bae65fb97042c9506076ad392135045f0129c380
Cuauhtemoctzin Rodriguez <ctrodriguez@ucdavis.edu>
2017-05-20 14:24:34 -0700

    Fixed the injector error. TODO apply conditional formatting again.

--- app/core/conditional-formatting/c-f.service.js
+++ app/core/conditional-formatting/c-f.service.js
@@ -1,49 +1,49 @@
 angular.module('core.cf').
     factory('cf', [
         function() {
-
+            var cf = {};
 
             // Easily make rgb obj
-            this.colsPoints = {};
+            cf.colsPoints = {};
             function rgb(r, g, b) {
                 return { r: r, g: g, b: b };
             }
 
             // Predefined colors
-            this.white = rgb(255, 255, 255);
-            this.blue = rgb(0, 0, 255);
-            this.red = rgb(255, 0, 0);
-            this.green = rgb(0,100,0);
-            this.purple = rgb(160,32,240);
+            cf.white = rgb(255, 255, 255);
+            cf.blue = rgb(0, 0, 255);
+            cf.red = rgb(255, 0, 0);
+            cf.green = rgb(0,100,0);
+            cf.purple = rgb(160,32,240);
 
-            this.init = function(column){
+            cf.init = function(column){
                 //All columns start showing conditional formating
                 column.showConditionalFormat = true;
             }
 
             // =====--- CONDITIONAL FORMATTING ---===== //
 
-            this.isUndefined = function(thing) {
+            cf.isUndefined = function(thing) {
                 return angular.isUndefined(thing);
             }
 
-            this.switchShowConditionalFormat = function(col){
+            cf.switchShowConditionalFormat = function(col){
                 col.showConditionalFormat = !col.showConditionalFormat;
             };
 
-            this.showFormattingSettings = function(col){
-                this.currentFormattingSettingsCol = col
+            cf.showFormattingSettings = function(col){
+                cf.currentFormattingSettingsCol = col
                 $(".formattingSettingsModal").modal();
             };
 
-            this.showHideSettingsButtons = function(){
-                this.showFormattingSettingsButtons = !this.showFormattingSettingsButtons;
-                //console.log(this.showFormattingSettingsButtons);
+            cf.showHideSettingsButtons = function(){
+                cf.showFormattingSettingsButtons = !cf.showFormattingSettingsButtons;
+                //console.log(cf.showFormattingSettingsButtons);
             };
 
-            this.submitFormattingSettings = function(col){
-                // this.maxAndMin[colName.name].max = document.getElementById("maxInput").value;
-                // this.maxAndMin[colName.name].min = document.getElementById("minInput").value;
+            cf.submitFormattingSettings = function(col){
+                // cf.maxAndMin[colName.name].max = document.getElementById("maxInput").value;
+                // cf.maxAndMin[colName.name].min = document.getElementById("minInput").value;
                 col.max = document.getElementById("maxInput").value;
                 col.min = document.getElementById("minInput").value;
                 col.maxColor = document.getElementById("maxColor").value;
@@ -52,9 +52,9 @@ angular.module('core.cf').
             };
 
 
-            this.conditionalFormat = function(value, col){
+            cf.conditionalFormat = function(value, col){
                 // Do nothing on bad/undef values
-                if(value == undefined || !value.good || this.maxAndMin[value.name] == undefined){
+                if(value == undefined || !value.good || cf.maxAndMin[value.name] == undefined){
                     return {};
                 }
 
@@ -65,13 +65,13 @@ angular.module('core.cf').
                 // Check is there is a user submitted max and min else use the max/ min of current data.
                 var max, min;
                 if(angular.isUndefined(col.max)){
-                    max = this.maxAndMin[value.name].max;
+                    max = cf.maxAndMin[value.name].max;
                 }
                 else{
                     max = Number(col.max)
                 }
                 if(angular.isUndefined(col.min)){
-                    min = this.maxAndMin[value.name].min;
+                    min = cf.maxAndMin[value.name].min;
                 }
                 else{
                     min = Number(col.min)
@@ -83,37 +83,37 @@ angular.module('core.cf').
                 // Check if user submitted color, if not default to red and blue for max and min respectivly.
                 var maxColor, minColor;
                 if(col.maxColor == "Red"){
-                    maxColor = this.red;
+                    maxColor = cf.red;
                 }
                 else if(col.maxColor == "Blue"){
-                    maxColor = this.blue;
+                    maxColor = cf.blue;
                 }
                 else if(col.maxColor == "Green"){
-                    maxColor = this.green;
+                    maxColor = cf.green;
                 }
                 else if(col.maxColor == "Purple"){
-                    maxColor = this.purple;
+                    maxColor = cf.purple;
                 }
 
                 if(col.minColor == "Red"){
-                    minColor = this.red;
+                    minColor = cf.red;
                 }
                 else if(col.minColor == "Blue"){
-                    minColor = this.blue;
+                    minColor = cf.blue;
                 }
                 else if(col.minColor == "Green"){
-                    minColor = this.green;
+                    minColor = cf.green;
                 }
                 else if(col.minColor == "Purple"){
-                    minColor = this.purple;
+                    minColor = cf.purple;
                 }
-                this.colsPoints[value.name] = [
+                cf.colsPoints[value.name] = [
                     { value: min, color: minColor},
-                    { value: (max+min)/2, color: this.white },
+                    { value: (max+min)/2, color: cf.white },
                     { value: max, color: maxColor},
                 ]
 
-                var color = gradient(this.colsPoints[value.name])(value.value);
+                var color = gradient(cf.colsPoints[value.name])(value.value);
 
                 var textColor = "white";
                 // Calculate overall intensity of color to determine text color
@@ -125,6 +125,8 @@ angular.module('core.cf').
                 return { "background-color": "rgb(" +color.r+ "," +color.g+ "," +color.b+ ")",
                         "color": textColor };
             }
+
+            return cf;
 
         }
     ]);
